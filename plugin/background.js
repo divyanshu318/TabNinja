3e71e2d925ff3ed2d44f6f64ed4edb76e4908a93
Snehal Tripathi <snehalprdc2003@gmail.com>
2024-06-07 23:14:28 +0530

    functions in backgorund

--- plugin/background.js
+++ plugin/background.js
@@ -212,3 +212,142 @@ const action = "null";
         sendAllTabs(1);
       });
     };
+
+    //function for Arranging tabs in LastAccessed Time
+    const sortTabsByLastAccessed = () => {
+      chrome.tabs.query({}, function (tabs) {
+        tabs.sort((a, b) => b.lastAccessed - a.lastAccessed);
+
+        tabs.forEach(function (tab, index) {
+          chrome.tabs.move(tab.id, { index: index });
+        });
+        sendAllTabs(1);
+      });
+    };
+
+    // Function to close all tabs except for a specific tab
+    const closeAllTabsExcept = (tabIdToKeep) => {
+      chrome.tabs.query({}, function (tabs) {
+        tabs.forEach(function (tab) {
+          if (tab.id !== tabIdToKeep) {
+            chrome.tabs.remove(tab.id);
+          }
+        });
+        closeExtensionMenu();
+      });
+    };
+
+    //check if bookmarked
+    const isTabBookmarked = (tabUrl) => {
+      return new Promise((resolve, reject) => {
+        chrome.bookmarks.search({ url: tabUrl }, (bookmarkNodes) => {
+          const isTabBookMarked = bookmarkNodes.length > 0;
+          resolve(isTabBookMarked);
+        });
+      });
+    };
+
+    //move tab to extreme right
+    const moveTabTOExtremeRight = (tabId) => {
+      chrome.tabs.move(tabId, { index: -1 });
+      sendAllTabs(1);
+    };
+
+    // function to remove duplicate tabs
+    const removeDuplicateTabs = (tabs) => {
+      tabs.forEach(function (tab) {
+        if (tab.duplicate) {
+          chrome.tabs.remove(tab.id);
+        }
+      });
+    };
+
+    //function close Extension Menu
+    const closeExtensionMenu = () => {
+      const tabsData = {
+        id: 10,
+      };
+      port.postMessage(tabsData);
+    };
+
+    //function to change currentWindowVariable
+    const changeCurrentWindowVariable = (value) => {
+      console.log(value);
+      currentWindow = value;
+      sendCurrentWindowVariable();
+      sendAllTabs(1);
+    };
+
+    //function to send value of currentWindowVariable
+    const sendCurrentWindowVariable = () => {
+      const currentWindowdata = {
+        id: 14,
+        currentWindow: currentWindow,
+      };
+      port.postMessage(currentWindowdata);
+    };
+
+    //function for bookamarking a tab
+    const bookamarkParticularTab = (tabId) => {
+      chrome.tabs.get(tabId, function (tab) {
+        if (tab) {
+          chrome.bookmarks.create(
+            { title: tab.title, url: tab.url },
+            function (bookmark) {}
+          );
+          sendAllTabs(1);
+        } else {
+          console.error("Tab with ID " + tabId + " not found");
+        }
+      });
+    };
+
+    //open tab in new window
+    const OpenTabInNewWindow = (tabId) => {
+      chrome.tabs.get(tabId, function (tab) {
+        if (tab) {
+          chrome.windows.create({
+            url: tab.url,
+          });
+        }
+      });
+    };
+
+    //open tab in icognito mode
+    const OpenTabInIncognitoMode = (tabId) => {
+      chrome.tabs.get(tabId, function (tab) {
+        if (tab) {
+          chrome.windows.create(
+            {
+              url: tab.url,
+              incognito: true,
+            },
+            function (newWindow) {}
+          );
+        }
+      });
+    };
+
+    //unbook mark a tab
+    const UnbookmarkTab = (tabUrl) => {
+      chrome.bookmarks.search({ url: tabUrl }, function (bookmarks) {
+        bookmarks.forEach(function (bookmark) {
+          chrome.bookmarks.remove(bookmark.id, function () {
+            console.log("Bookmark removed for tab:", tabId);
+          });
+        });
+        sendAllTabs(1);
+      });
+    };
+
+    //star marking a tab
+    const starMarkATab = (tabId) => {
+      starTabs.set(tabId, 1);
+      sendAllTabs(1);
+    };
+
+    //star Unmarking a tab
+    const starUnmarkAtab = (tabId) => {
+      starTabs.delete(tabId);
+      sendAllTabs(1);
+    };
